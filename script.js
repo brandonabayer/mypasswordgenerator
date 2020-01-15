3cc7b6418aba9d93bacf87bc927fc75e0bc9b2bf
Brandon Bayer <brandonabayer@gmail.com>
2020-01-14 18:31:45 -0600

    variables with correct array format

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var upperCase = "char2";
 var specialCase = "char3";
 var numericCase = "char4";
 
-var lowerChar = [abcdefghijklmnopqrstuvwxyz];
-var upperChar = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
-var specialChar = [];
-var numericChar = [12334567890];
+var lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specialChar = ['!','@','#','$','%','^','&','*','(',')','-','+','=','?','~',';',':','{','}','[',']'];
+var numericChar = ['1','2','3','3','4','5','6','7','8','9','0'];
 
 function myFunction() {
     var x, text;

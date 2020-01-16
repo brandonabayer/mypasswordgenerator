0b372c86f256328d5d1a2e28c7853992ab04ac3f
Brandon Bayer <brandonabayer@gmail.com>
2020-01-16 13:47:09 -0600

    mypassword generator working

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
-var lowerCase = "char1";
-var upperCase = "char2";
-var specialCase = "char3";
-var numericCase = "char4";
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-var lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var specialChar = ['!','@','#','$','%','^','&','*','(',')','-','+','=','?','~',';',':','{','}','[',']'];
-var numericChar = ['1','2','3','3','4','5','6','7','8','9','0'];
 
-function myFunction() {
-    var x, text;
-    
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-    // If x is Not a Number or less than 8 or greater than 128
-    if (isNaN(x) || x < 8 || x > 128) {
-        text = "Input not valid";
-    } else {
-        ;
-    };
-    document.getElementById("generator").innerHTML = text;
+    passwordText.value = password;
+
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
 }
+
+function copyToClipboard() {
+    // BONUS 
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
+function generatePassword() {
+    var pLength = prompt("how long do you want the password to be? choose between 8-128")
+    var valid = false;
+
+    while (!valid) {
+        if (
+            pLength < 7
+            || pLength > 128
+            || isNaN(parseInt(pLength))
+        ) {
+            pLength = prompt("invalid try again. choose between 8-128")
+        }
+        else {
+            valid = true;
+        }
+    }
+    console.log(pLength);
+    var specialCharacters = confirm("Do you want special characters?");
+    var numericCharacters = confirm("Do you want numeric characters?");
+    var lowercaseCharacters = confirm("Do you want lowercase characters?");
+    var uppercaseCharacters = confirm("Do you want uppercase characters");
+
+    if (specialCharacters) {
+        var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+    }
+    if (numericCharacters) {
+        var numbers = '0123456789';
+    }
+    if (lowercaseCharacters) {
+        var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    }
+    if (uppercaseCharacters) {
+        var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    var all = uppercase + lowercase + numbers + symbols;
+    console.log(all);
+    var password = '';
+    for (var index = 0; index < pLength; index++) {
+        var randomNumber = Math.floor(Math.random() * all.length);
+        console.log(randomNumber);
+        password += all.substring(randomNumber, randomNumber + 1);
+        console.log(password);
+    }
+    return password;
+}
+generateBtn.addEventListener("click", writePassword);
+document.getElementById("generator").innerHTML = text;
